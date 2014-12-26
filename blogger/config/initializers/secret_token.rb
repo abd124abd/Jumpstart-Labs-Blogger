c2bbac04bf46417d42575705860141397a801c0e
Ali Ayoub <aliayoub22@gmail.com>
2014-12-25 18:28:45 -0600

    Stage 1 commit of changes
    - ArticlesController with some actions and routing
    
    - CSS styling

---
+++ blogger/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = '706d2fb8ab4420f0eca9bffca16666c835d246d7923649c22a256c017cdb16378acf2fdecab3c20e857a59be271fc406d54b9fa70e40bfe87ab6de45f1e57970'
